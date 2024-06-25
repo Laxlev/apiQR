bc82ca42bfbefedf6c07716db7556df9d79440c5
Laxlev <leofrivgames1@gmail.com>
2024-06-25 12:17:51 -0600

    chore: Update .gitignore and index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "./QR.enviroments.js";
+import { firebaseConfig } from "./.env";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { collection } from "firebase/firestore";
 import express from 'express';
@@ -12,13 +12,20 @@ const port = server.get('port') || 1722;
 
 const db = getFirestore(app);
 
-const collectionRef = collection(db, 'citas');
-const snapshot = await getDocs(collectionRef);
-snapshot.forEach(doc => {
-    console.log(doc.id, '=>', doc.data());
-});
-
-
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
+});
+
+server.get('/users/:id', async (req, res) => {
+    const id = req.params.id;
+    const ref = doc(db, 'users', id);
+    const docSnap = await getDoc(ref);
+
+    if (docSnap.exists()) {
+        console.log('Document data:', docSnap.data());
+        res.json(docSnap.data());
+    }else{
+        console.log('No such document!');
+        res.json({error: 'Usuario no encontrado!'});
+    }
 });
