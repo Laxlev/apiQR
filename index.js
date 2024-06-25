4c233919d51be42aa9c86fd110bb698fb0885126
Laxlev <leofrivgames1@gmail.com>
2024-06-25 13:29:44 -0600

    chore: Update firebase initialization and error handling in index.js

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import express from 'express';
 
-// Initialize Firebase Admin SDK
-const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const serviceAccount = {
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key,
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url,
+    universe_domain: process.env.universe_domain
+};
+
 initializeApp({
-  credential: cert(serviceAccount)
+credential: cert(serviceAccount)
 });
 
 const db = getFirestore();
